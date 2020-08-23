133c57157dd9fa61a66d5c4e29ba64c08884864b
openqubeio <hola@openqube.io>
2020-08-23 19:41:08 -0300

    Adding encuesta 2020.02

---
+++ encuesta-sueldos-2020.02/precache-manifest.6feb4ce79500dc7dfd4e23849715665c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2be11a857e84fae22cf",
+    "url": "/static/css/main.bb384ad1.chunk.css"
+  },
+  {
+    "revision": "b2be11a857e84fae22cf",
+    "url": "/static/js/main.b2be11a8.chunk.js"
+  },
+  {
+    "revision": "df6db29a9bce8802c011",
+    "url": "/static/js/1.df6db29a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0ec26a5850744661b2b5ec8aea65ac8",
+    "url": "/index.html"
+  }
+];
