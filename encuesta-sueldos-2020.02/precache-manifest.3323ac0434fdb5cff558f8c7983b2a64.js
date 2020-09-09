b5fa2442444958915b903287746d7e53264991c1
Ariel Jolo <ajolo@sysarmy.com.ar>
2020-09-09 01:23:03 -0300

    fixing static assets

---
+++ encuesta-sueldos-2020.02/precache-manifest.3323ac0434fdb5cff558f8c7983b2a64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ae6b074d77cf52d2fed",
+    "url": "/static/css/main.fa34b900.chunk.css"
+  },
+  {
+    "revision": "6ae6b074d77cf52d2fed",
+    "url": "/static/js/main.6ae6b074.chunk.js"
+  },
+  {
+    "revision": "df6db29a9bce8802c011",
+    "url": "/static/js/1.df6db29a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b7211da1add36a0f1d7e046fc82d4cc",
+    "url": "/index.html"
+  }
+];
--- encuesta-sueldos-2020.02/service-worker.js
+++ encuesta-sueldos-2020.02/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6feb4ce79500dc7dfd4e23849715665c.js"
+  "/precache-manifest.3323ac0434fdb5cff558f8c7983b2a64.js"
 );
 
 workbox.clientsClaim();
