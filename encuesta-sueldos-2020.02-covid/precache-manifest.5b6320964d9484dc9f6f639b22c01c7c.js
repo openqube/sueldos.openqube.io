e8625005f3b7ca393477491b561a2a76fdb5cd5c
Ariel Jolo <ajolo@sysarmy.com.ar>
2020-09-09 00:51:01 -0300

    Adding covid results

---
+++ encuesta-sueldos-2020.02-covid/precache-manifest.5b6320964d9484dc9f6f639b22c01c7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a69053ec8ac951b0e96",
+    "url": "/static/css/main.d33a6ce6.chunk.css"
+  },
+  {
+    "revision": "6a69053ec8ac951b0e96",
+    "url": "/static/js/main.6a69053e.chunk.js"
+  },
+  {
+    "revision": "3dd8f580613fe38319f2",
+    "url": "/static/js/1.3dd8f580.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa8c6fbe770c2a0e916facd0a05ea468",
+    "url": "/index.html"
+  }
+];
