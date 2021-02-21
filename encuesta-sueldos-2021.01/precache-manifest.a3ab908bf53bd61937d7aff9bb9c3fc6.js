392712817174aceef477f1a7faa057fe1090689e
openqubeio <info@openqube.io>
2021-02-21 20:20:56 -0300

    add 2021.01

---
+++ encuesta-sueldos-2021.01/precache-manifest.a3ab908bf53bd61937d7aff9bb9c3fc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf624ecda39ebcb07a12",
+    "url": "/static/css/main.6caedf69.chunk.css"
+  },
+  {
+    "revision": "bf624ecda39ebcb07a12",
+    "url": "/static/js/main.bf624ecd.chunk.js"
+  },
+  {
+    "revision": "3b360997d61f68c228e6",
+    "url": "/static/js/1.3b360997.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8563d546bf43aba150d61fdcc4a0a28b",
+    "url": "/index.html"
+  }
+];
