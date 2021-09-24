0edd5d92b26e24b949db340da7f6b0d4a7a74e02
Ariel Jolo <ajolo@sysarmy.com>
2021-09-24 01:23:02 -0300

    Adding 2021.02

---
+++ encuesta-sueldos-2021.02/precache-manifest.fb098729b965c57fdf88abf38dd49a0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd1a02e4fc88dbf06351",
+    "url": "./static/css/main.4a4e5002.chunk.css"
+  },
+  {
+    "revision": "dd1a02e4fc88dbf06351",
+    "url": "./static/js/main.dd1a02e4.chunk.js"
+  },
+  {
+    "revision": "cb45a5c5d01360807413",
+    "url": "./static/js/1.cb45a5c5.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d4bf69f24867d66d4cd60f6f917aed85",
+    "url": "./index.html"
+  }
+];
